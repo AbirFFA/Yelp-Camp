41e502512798c2c40cdba1334950de5a475e2394
Fahim Faisal <fahim07@student.sust.edu>
2019-09-17 23:14:07 +0600

    User profile implemented
    Also tried to decorate user profile.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require("express"),
     router  = express.Router(),
     passport = require("passport"),
-    User = require("../models/users");
+    User = require("../models/users"),
+    Campground = require("../models/campground");
 
 router.get("/", function(req, res){
     res.render("landing");
@@ -12,7 +13,13 @@ router.get("/register", function (req, res) {
 });
 
 router.post("/register", function (req, res) {
-    var newUser = new User({username: req.body.username});
+    var newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        avatar: req.body.avatar
+    });
 
     if (req.body.adminCode === "itsacode") {
         newUser.isAdmin = true;
@@ -45,6 +52,25 @@ router.get("/logout", function (req, res) {
     req.logout();
     req.flash('success', 'You are signed out.');
     res.redirect("/");
+});
+
+router.get("/user/:id", function (req, res) {
+    User.findById(req.params.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+            req.flash("error", "Something went wrong");
+            res.redirect("back");
+        }
+
+        Campground.find().where("author.id").equals(foundUser._id).exec(function (err, campgrounds) {
+            if (err) {
+                console.log(err);
+                req.flash("error", "Something went wrong");
+                res.redirect("back");
+            }
+            res.render("users/user", {user: foundUser, campgrounds: campgrounds});
+        });
+    });
 });
 
 module.exports = router;
